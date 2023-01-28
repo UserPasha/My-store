a26f2a62df53f11bc28e74e576ded28124211281
UserPasha <pasharoma13@gmail.com>
2023-01-28 13:18:26 +0300

    FIX change cart styles

--- app/Components/Cart/Cart.tsx
+++ app/Components/Cart/Cart.tsx
@@ -2,32 +2,45 @@ import React, {FC, useRef, useState} from 'react';
 import style from './Cart.module.scss'
 import {CartItem} from "../CartItem/CartItem";
 import {cart} from "../CartItem/DataCart";
+import Drawer from "../Drawer/Drawer";
 
 export const Cart: FC = () => {
 
     const [isOpen, setIsOpen] = useState<boolean>(false)
-    const locker = () =>{
+    const locker = () => {
         setIsOpen(!isOpen)
     }
     const buttonRef = useRef<HTMLButtonElement>(null)
+    const [drawerIsOpen, setDrawerIsOpen] = useState<boolean>(false)
 
     return (
         <div className={style.wrapper}>
             <button className={style.heading}
-            onClick={locker}
-            ref={buttonRef}>
+                    onClick={locker}
+                    ref={buttonRef}>
             <span className={style.badge}>
                  1
             </span>
-            <span className={style.basket}>
+                <span className={style.basket}>
                   My basket
             </span>
             </button>
-            {isOpen &&  <div className={style.menu}>
-                {cart.map((item, index)=>
+            {/*{isOpen && <div className={style.menu}>*/}
+            {/*    <button className={style.closedButton} onClick={locker}> X</button>*/}
+            {/*    {cart.map((item, index) =>*/}
+            {/*        <CartItem key={index} item={item}/>*/}
+            {/*    )}*/}
+
+            {/*</div>}*/}
+            {isOpen ? <div className={style.menu}>
+                <button className={style.closedButton} onClick={locker}> X</button>
+                {cart.map((item, index) =>
                     <CartItem key={index} item={item}/>
                 )}
-            </div>}
+            </div>
+                :
+                <div className={style.closed}></div>}
+
 
         </div>
     );
