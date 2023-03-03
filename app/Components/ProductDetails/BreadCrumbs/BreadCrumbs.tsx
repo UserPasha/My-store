2c520303ee4149df9f2e2f80061335370feae80e
UserPasha <pasharoma13@gmail.com>
2023-03-03 17:55:44 +0300

    FEAT BreadCrumbs

--- app/Components/ProductDetails/BreadCrumbs/BreadCrumbs.tsx
+++ app/Components/ProductDetails/BreadCrumbs/BreadCrumbs.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './BreadCrumbs.module.scss'
+import {useRouter} from "next/router";
+import Link from "next/link";
+
+const convertBreadcrumb = (string:string) => {
+    return string
+        .replace(/-/g, ' ')
+        .replace(/oe/g, 'ö')
+        .replace(/ae/g, 'ä')
+        .replace(/ue/g, 'ü')
+        .toUpperCase();
+};
+
+interface ICrumb
+{ breadcrumb: string
+    href: string; }
 
 
 export const BreadCrumbs = () => {
+
+    const router = useRouter();
+    const [breadcrumbs, setBreadcrumbs] = useState<ICrumb[] | null>(null);
+
+
+    useEffect(() => {
+        if (router) {
+            const linkPath = router.asPath.split('/');
+            linkPath.shift();
+
+            const pathArray = linkPath.map((path, i) => {
+                return { breadcrumb: path, href: '/' + linkPath.slice(0, i + 1).join('/') };
+            });
+
+            setBreadcrumbs(pathArray);
+
+        }
+    }, [router]);
+
+    if (!breadcrumbs) {
+        return null;
+    }
+
     return (
-        <div>
-4-41-43
-        </div>
+        <nav aria-label="breadcrumbs">
+            <ol className={style.ol}>
+                <li>
+                    <a href="/">HOME/</a>
+                </li>
+                {breadcrumbs.map((breadcrumb, i) => {
+                    return (
+                        <li key={breadcrumb.href}>
+                            <Link href={breadcrumb.href} legacyBehavior>
+                                <a  className={style.one}>
+                                    {`${convertBreadcrumb(breadcrumb.breadcrumb)}/`}
+                                </a>
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ol>
+        </nav>
+//         <div>
+// 4-41-43
+//         </div>
     );
 };
 
--- app/Components/ProductDetails/ProductDetails.tsx
+++ app/Components/ProductDetails/ProductDetails.tsx
@@ -3,12 +3,14 @@ import style from './ProductDetails.module.scss'
 import {IProduct} from "../../types/InterfaceProduct";
 import {IProductDetails} from "../../../pages/product/[slug]";
 import {Title} from "../Catalog/Title/Title";
+import {BreadCrumbs} from "./BreadCrumbs/BreadCrumbs";
 
 export const ProductDetails:FC<IProductDetails> = ({product}) => {
     return (
         <div>
             <Title>
                 {product.name}
+                <BreadCrumbs/>
             </Title>
         </div>
     );
